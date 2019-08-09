3e5ab1ebf8ab2586a233a6003ff23c32a38689b9
Емельянов Александр Геннадьевич <A.Emelianov@gazprom-media.com>
2019-08-09 15:43:25 +0300

    скорость кликов

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,9 +1,27 @@
 const image = document.getElementById('cookie');
 const counter = document.getElementById('clicker__counter');
-function changeSize() {
-image.width = 220;
-counter.textContent ++;
-setTimeout(() => image.width = 200, 10);
-};
+const speed = document.getElementById('speed__counter');
 
-image.onclick = changeSize; 
+function changeSize() {
+    image.width = 220;
+    counter.textContent ++;
+    setTimeout(() => image.width = 200, 10);
+    speedCount();
+}
+
+let time = [];
+console.log(time.length);
+function speedCount() {
+    if (image.onclick) {
+        if (time.length == 0) {
+            time.push(Date.now());
+            speed.textContent = 1 / ( time[time.length - 1]/1000);
+        }
+    
+    else if (time.length > 0) {
+        time.push(Date.now());
+        speed.textContent = 1 / (( time[time.length - 1] - time[time.length - 2])/1000);
+    }}
+}
+
+image.onclick = changeSize;

a8cc357000f35ff750302b682cb58635a6513d70
Емельянов Александр Геннадьевич <A.Emelianov@gazprom-media.com>
2019-08-15 10:57:22 +0300

    2.0

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -3,25 +3,26 @@ const modalSuccess = document.getElementById('modal_success');
 const close = document.querySelectorAll('div.modal__close');
 const success = document.querySelector('a.show-success');
 
-function modal() {
-    modalMain.className = 'modal modal_active';
-
-    close.item(0).onclick = function() {
-        modalMain.className = 'modal';
-    }
-
-    success.onclick = function () {
-        modalMain.className = 'modal';
-        modalSuccess.className = 'modal modal_active';
-    }
-
-    modalSuccess.onclick = function() {
-        modalSuccess.className = 'modal';
-    }
-
-    close.item(1).onclick = function() {
+function closeModal() {
+    modalMain.className = 'modal';
     modalSuccess.className = 'modal';
+    modalSuccess.className = 'modal';
+};
+
+function openModal(modal) {
+        modal.className = 'modal modal_active';
+        modal.className = 'modal modal_active';
+}
+
+function modal() {
+    openModal(modalMain)
+    close.item(0).onclick = closeModal;
+    success.onclick = function() {
+         closeModal();
+         openModal(modalSuccess);
     }
+    modalSuccess.onclick = closeModal;
+    close.item(1).onclick = closeModal;
 }
 
 modal();

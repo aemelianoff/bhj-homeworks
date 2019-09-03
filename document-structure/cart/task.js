71970190510f492076d746b2b824b249a8dc7c0d
Емельянов Александр Геннадьевич <A.Emelianov@gazprom-media.com>
2019-09-03 14:53:53 +0300

    1.2

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -2,12 +2,12 @@ const cartProducts = document.querySelector('.cart__products');
 class addToCart {
     constructor(container) {
     this.container = container;
-    this.Quantity = container.querySelector('.product__quantity-value');
+    this.quantity = container.querySelector('.product__quantity-value');
     this.quantityDec = container.querySelector('.product__quantity-control.product__quantity-control_dec');
     this.quantityInc = container.querySelector('.product__quantity-control.product__quantity-control_inc');
     this.productImage = container.querySelector('.product__image');
     this.add = container.querySelector('.product__add');
-    this.count = this.Quantity.textContent;
+    this.count = this.quantity.textContent;
 
     this.registerEvents();
 }
@@ -19,27 +19,27 @@ class addToCart {
         let cartProduct = cartProducts.querySelectorAll('.cart__product');
         for (let product of Array.from(cartProduct)) {
             if (product.dataset.id === id) {
-                let addToCart = parseInt(this.Quantity.textContent) + parseInt(product.children[1].textContent);
+                let addToCart = parseInt(this.quantity.textContent) + parseInt(product.children[1].textContent);
                 product.children[1].innerText = addToCart;
                 return false;
             }
         }
         cartProducts.insertAdjacentHTML('beforeEnd', `<div class='cart__product' data-id='${id}'>
                                         <img class='cart__product-image' src='${this.productImage.src}'>
-                                        <div class='cart__product-count'>${this.Quantity.textContent}</div>
+                                        <div class='cart__product-count'>${this.quantity.textContent}</div>
                                         </div>`);
         });
 
         this.quantityDec.addEventListener( 'click', () => {
             if (this.count > 1) {
                 this.count--;
-                this.Quantity.textContent = this.count;
+                this.quantity.textContent = this.count;
             }
         })
 
         this.quantityInc.addEventListener( 'click', () => {
                 this.count++;
-                this.Quantity.textContent = this.count;
+                this.quantity.textContent = this.count;
             });
         
 
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,16 +4,19 @@ for (let tip of hasTooltip) {
     tip.addEventListener('click', (e) => {
         e.preventDefault();
         const title = e.target.getAttribute('title');
-        if (document.querySelector('.tooltip_active')) {
-            if (document.querySelector('.tooltip_active').textContent != title) {
-                document.querySelector('.tooltip_active').remove();
-                tip.insertAdjacentHTML("afterEnd", `<div class="tooltip tooltip_active">${title}</div>`);
+        const toolTipActive = document.querySelector('.tooltip_active');
+        if (toolTipActive) {
+            if (toolTipActive.textContent != title) {
+                toolTipActive.remove();
+                tip.insertAdjacentHTML("afterEnd", `<div class="tooltip tooltip_active" style="left: ${tip.getBoundingClientRect().left}px; top: ${tip.getBoundingClientRect().top + 20}px">${title}</div>`);
+                //console.log(tip.getBoundingClientRect().left);
             }
             else {
-                document.querySelector('.tooltip_active').remove();}
+                toolTipActive.remove();}
         }
         else  {
-            tip.insertAdjacentHTML("afterEnd", `<div class="tooltip tooltip_active">${title}</div>`);
+            tip.insertAdjacentHTML("afterEnd", `<div class="tooltip tooltip_active" style="left: ${tip.getBoundingClientRect().left}px; top: ${tip.getBoundingClientRect().top + 20}px">${title}</div>`);
         }
     });
+    
 }
